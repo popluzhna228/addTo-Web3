78f7d405ee5c0ce31c1f6357281bea3a2124f423
popluzhna228 <116947601+popluzhna228@users.noreply.github.com>
2023-07-16 17:44:07 +0600

    Update web3.js

--- web3.js
+++ web3.js
@@ -3,3 +3,4 @@ const { Web3Storage } = require('web3.storage')
 
 async function addToWeb3 ({ endpoint, token, pathToAdd, name, wrapWithDirectory = false, includeHidden }) {
   const web3 = new Web3Storage({ endpoint, token })
+  const files = await getFilesFromPath(pathToAdd, { hidden: includeHidden })
